fe0417c1d11c77bd163f86c911143819a868bcbd
Anthony Crawford <crawfishmedia@gmail.com>
2024-11-22 02:18:38 -0400

    get links in description and comments

--- odysee.py
+++ odysee.py
@@ -23,24 +23,56 @@ from os.path import splitext
 from tqdm import tqdm
 import requests
 import re
+from playwright.sync_api import sync_playwright
+
+print()
 
 
 ## Options
 get_links = True
+download_files = False
 encode_video = False
+links_file_init = False
 
 
+def block_aggressively(route): 
+	if (route.request.resource_type != "document"): 
+		route.abort() 
+	else: 
+		route.continue_()
+
+# Get React HTML
+def fetch_html(url):
+	with sync_playwright() as p:
+		# Launch a browser
+		browser = p.firefox.launch(headless=True)  # Set headless=False if you want to see the browser
+
+		page = browser.new_page()
+		page.set_default_navigation_timeout(30000.0)
+		page.on("request", lambda request: print(">>", request.method, request.url))
+		page.on("response", lambda response: print("<<", response.status, response.url))
+		page.route(re.compile(r"\.(qt|mov|mp4|jpg|png|svg|webp|wott)$"), lambda route: route.abort()) 
+
+		# Navigate to the desired URL
+		page.goto(url)
+
+		# Wait for the React app to load completely
+		page.wait_for_load_state('networkidle')
+
+		# Get the HTML content of the page
+		html_content = page.content()
+		# Close the browser
+		browser.close()
+		return html_content
+
+
+# Remove all special characters except spaces and alphanumeric characters
+# Hyphens, commas, periods allowed
 def clean_text(text):
-    # Removes all special characters except spaces and alphanumeric characters
-    # Hyphens, commas, periods allowed
-    clean = re.sub(r'[^\~]', '-', text)
-    cleaned = re.sub(r'[^a-zA-Z0-9\s\,\-\.]', '', clean)
-    return cleaned
-
-
-downloads_dir = 'downloads'
-if not os.path.exists(downloads_dir):
-	os.makedirs(downloads_dir)
+	clean = re.sub(r'[^a-zA-Z0-9\s\,\-\.\~\&]', '', text)
+	clean = clean.replace('~','-')
+	cleaned = clean.replace('/','-')
+	return cleaned.strip()
 
 
 # get the list of URLs to grab
@@ -55,154 +87,189 @@ dld.close()
 
 # Get metadata from urls
 for url in dld_urls:
-	try:
-		rs = requests.Session()
-		response = rs.get(url, timeout=20)
-		response.raise_for_status()
-		content = response.content
-		rs.close()
-	except requests.exceptions.HTTPError as err:
-		print(f'HTTP Error: {err}')
-	except requests.exceptions.ConnectionError as errc:
-		print(f'Error Connecting: {errc}')
-	except requests.exceptions.Timeout as errt:
-		print(f'Timeout Error: {errt}')
-	except requests.exceptions.RequestException as errr:
-		print(f'OOps: Something Else: {errr}')				
-	finally:
-		rs.close()
 
-	# If no metadata then goto next url
+	## Grab the HTML of the Odysee video post
+	print();print();print("Grabbing Odysee HTML...")
+	print(url);print()
+
+	content = fetch_html(url)
+	
+	## Testing
+	#content = open('test.html', 'r')
+
+	# If no content then try next URL
 	if not content:
 		continue
 
-	# Parse webpage JSON metadata with bs4
+	# Parse webpage JSON metadata
 	soup = BeautifulSoup(content, 'html.parser')
 	script_tag = soup.find('script', {'type': 'application/ld+json'})
+
 	if script_tag:
 		json_content = json.loads(script_tag.string)
+		#print(json_content)
 	else:
-		print;print('Script tag content not found.');print
-
-	#print(json_content)
-
-	chunk_size = 1024
+		print();print('Script tag for metadata not found.');print()
 
 	# Parse
 	title = json_content['name']
+	title_for_links = title
 	title = clean_text(title)
-	title = title.strip()
 	thumb = json_content['thumbnailUrl']
 	video = json_content['contentUrl']
 	upload_date = json_content['uploadDate']
-
-	print()
-	print("Title: " + title)
-	print("Thumb: " + thumb)
-	print("Video: " + video)
 
 	# Grab date for filenames
 	uploaded = datetime.strptime(upload_date, '%Y-%m-%dT%H:%M:%S.%fZ')
 	created = uploaded.strftime("%b %-d %Y").lower()  # Format: nov 1 2024
 
+	# Create new text file for today's links
+	# Format: 'Psinergy Links Nov 1 2024.txt'
+	if not links_file_init:
+		created_links = uploaded.strftime("%B %-d %Y") # Format: November 1 2024
+		links_filename = "Psinergy Links " + created_links + ".txt"
+		print();print("Links file: " + links_filename)
+		with open(links_filename, 'w') as nf:
+			nf.write("Psinergy Links " + created_links + '\n\n')
+		nf.close()
+		links_file_init = True
+
+	# Show details for this URL
+	print()
+	print("Title: " + title_for_links)
+	print("Thumb: " + thumb)
+	print("Video: " + video)
+
+
 	# Get links from HTML
-	# if get_links:
-	# 	links = []
-	# 	created_links = uploaded.strftime("%B %-d %Y") # Format: November 1 2024
+	if get_links:
+
+		# Initialize for this URL
+		links = []
 		
-	# 	# Description
-	# 	desc_links = soup.find('script', {'type': 'application/ld+json'})
-	# 	for link in desc_links:
-	# 		links.append(link.strip())
+		# Description
+		print();print("Description links:")
+		description_element = soup.find('div', class_='markdown-preview--description')
+		if not description_element:
+			print("Could not find the Description class.")
+		else:
+			desc_links = description_element.find_all('a')
+			for link in desc_links:
+				link_http = link.get('href')
+				if link_http.startswith('http') or link_http.startswith('https'):
+					print(link.get('href'))
+					links.append(link.get('href'))
 
-	# 	# Comments from Psinergy only
-	# 	comment_links = soup.find('script', {'type': 'application/ld+json'})
-	# 	for link in comment_links:
-	# 		links.append(link.strip())
+		# Comments from Psinergy only:
+		# Biofield Practice 0nly ~ Psinergy
+		# Psinergy0Nhold
+		print();print("Comments links:")
+		comments_element = soup.find('ul', class_='comments')
+		if not comments_element:
+			print("Couldn't find any comments.")
+		else:
+			comment_links = comments_element.find_all('a')
+			for link in comment_links:
+				link_http = link.get('href')
+				if link_http.startswith('http') or link_http.startswith('https'):
+					print(link.get('href'))
+					links.append(link.get('href'))
 
-	# 	# Create text file for today's links
-	# 	links_filename = "Psinergy Links " + created_links + ".txt"
-	# 	with open(links_filename, 'a') as lf:
-	# 		lf.write('\n\n\n\n' + title + '\n\n')
-	# 		for link in links:
-	# 			lf.write(link+'\n')
-	# 	lf.close()
+		# Append the links to text file from this URL
+		if len(links) > 0:
+			with open(links_filename, 'a') as lf:
+				lf.write(title_for_links + '\n\n')
+				for link in links:
+					lf.write(link+'\n')
+				lf.write('\n\n')
+			lf.close()
 
-	# quit()
 
+	## Download files
+	if download_files:
 
-	# Get Thumb
-	try:
-		rs = requests.Session()
-		response = rs.get(thumb, timeout=20, allow_redirects=True)
-		response.raise_for_status()
-		total_size = int(response.headers.get('content-length', 0))
+		# Prep downloads folder
+		downloads_dir = 'downloads'
+		if not os.path.exists(downloads_dir):
+			os.makedirs(downloads_dir)
 
-		print(f'Downloading Thumbnail: {url}')
+		chunk_size = 1024
+
+		# Get Thumb
+		try:
+			rs = requests.Session()
+			response = rs.get(thumb, timeout=20, allow_redirects=True)
+			response.raise_for_status()
+			total_size = int(response.headers.get('content-length', 0))
+
+			print(f'Downloading Thumbnail: {url}')
+			
+			filename = thumb.split('/')[-1]
+			new_fn = title + " " + created + '.jpg'
+
+			with open(new_fn, 'wb') as file, tqdm(desc='Progress', total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as bar:
+				for chunk in response.iter_content(chunk_size=chunk_size):
+					if chunk:
+						file.write(chunk)
+						bar.update(len(chunk))
+			shutil.move(new_fn, os.path.join(downloads_dir, new_fn))
+			file.close()
+
+		except requests.exceptions.HTTPError as err:
+			print(f'HTTP Error: {err}')
+		except requests.exceptions.ConnectionError as errc:
+			print(f'Error Connecting: {errc}')
+		except requests.exceptions.Timeout as errt:
+			print(f'Timeout Error: {errt}')
+		except requests.exceptions.RequestException as errr:
+			print(f'Oops: Something Else: {errr}')				
+		finally:
+			rs.close()
+
 		
-		filename = thumb.split('/')[-1]
-		new_fn = title + " " + created + '.jpg'
+		# Get Video
+		try:
+			rs = requests.Session()
+			response = rs.get(video, timeout=20, stream=True, allow_redirects=True)
+			response.raise_for_status()
+			total_size = int(response.headers.get('content-length', 0))
+			
+			print(f'Downloading Video: {url}')
 
-		with open(new_fn, 'wb') as file, tqdm(desc='Progress', total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as bar:
-			for chunk in response.iter_content(chunk_size=chunk_size):
-				if chunk:
-					file.write(chunk)
-					bar.update(len(chunk))
-		shutil.move(new_fn, os.path.join(downloads_dir, new_fn))
-		file.close()
+			filename = video.split('/')[-1]
+			file, ext = os.path.splitext(filename)
+			new_fn = title + " " + created + ext
+			
+			with open(new_fn, 'wb') as file, tqdm(desc='Progress', total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as bar:
+				for chunk in response.iter_content(chunk_size=chunk_size):
+					if chunk:
+						file.write(chunk)
+						bar.update(len(chunk))
+			
+			shutil.move(new_fn, os.path.join(downloads_dir, new_fn))
+			file.close()
 
-	except requests.exceptions.HTTPError as err:
-		print(f'HTTP Error: {err}')
-	except requests.exceptions.ConnectionError as errc:
-		print(f'Error Connecting: {errc}')
-	except requests.exceptions.Timeout as errt:
-		print(f'Timeout Error: {errt}')
-	except requests.exceptions.RequestException as errr:
-		print(f'Oops: Something Else: {errr}')				
-	finally:
-		rs.close()
+		except requests.exceptions.HTTPError as err:
+			print(f'HTTP Error: {err}')
+		except requests.exceptions.ConnectionError as errc:
+			print(f'Error Connecting: {errc}')
+		except requests.exceptions.Timeout as errt:
+			print(f'Timeout Error: {errt}')
+		except requests.exceptions.RequestException as errr:
+			print(f'Oops: Something Else: {errr}')
+		finally:	
+			rs.close()
 
-	
-	# Get Video
-	try:
-		rs = requests.Session()
-		response = rs.get(video, timeout=20, stream=True, allow_redirects=True)
-		response.raise_for_status()
-		total_size = int(response.headers.get('content-length', 0))
-		
-		print(f'Downloading Video: {url}')
 
-		filename = video.split('/')[-1]
-		file, ext = os.path.splitext(filename)
-		new_fn = title + " " + created + ext
-		
-		with open(new_fn, 'wb') as file, tqdm(desc='Progress', total=total_size, unit='B', unit_scale=True, unit_divisor=1024) as bar:
-			for chunk in response.iter_content(chunk_size=chunk_size):
-				if chunk:
-					file.write(chunk)
-					bar.update(len(chunk))
-		
-		shutil.move(new_fn, os.path.join(downloads_dir, new_fn))
-		file.close()
-
-	except requests.exceptions.HTTPError as err:
-		print(f'HTTP Error: {err}')
-	except requests.exceptions.ConnectionError as errc:
-		print(f'Error Connecting: {errc}')
-	except requests.exceptions.Timeout as errt:
-		print(f'Timeout Error: {errt}')
-	except requests.exceptions.RequestException as errr:
-		print(f'Oops: Something Else: {errr}')
-	finally:	
-		rs.close()
-
-print()
-
+# Move the links file to 'downloads'
+#shutil.move(links_filename, os.path.join(downloads_dir, links_filename))
 
 
 ### Encode Video Files ###
 if not encode_video:
 	quit()
+
+print()
 
 # Directory with video files
 output_dir = 'encodes'
